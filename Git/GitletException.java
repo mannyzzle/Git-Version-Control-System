5f0cc808b6d2c845bfc36d701258b367988de763
Enmanuel Hernandez <112276581+mannyzzle@users.noreply.github.com>
2024-05-02 22:46:36 -0700

    Update GitletException.java

--- Git/GitletException.java
+++ Git/GitletException.java
@@ -1,8 +1,6 @@
 package gitlet;
 
-/** General exception indicating a Gitlet error.  For fatal errors, the
- *  result of .getMessage() is the error message to be printed.
- *  @author P. N. Hilfinger
+/** General exception indicating a Gitlet error. 
  */
 class GitletException extends RuntimeException {
 
